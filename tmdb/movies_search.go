f4237216ff5a6f3a05525dcc00009a10ba6e0aa6
nichuanfang <f18326186224@gmail.com>
2024-08-29 00:26:46 +0800

    perf: 优化搜索排序

--- tmdb/movies_search.go
+++ tmdb/movies_search.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fengqi/kodi-metadata-tmdb-cli/utils"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 func (t *tmdb) SearchMovie(chsTitle, engTitle string, year int) (*SearchMoviesResults, error) {
@@ -74,10 +76,59 @@ func (t *tmdb) SearchMovie(chsTitle, engTitle string, year int) (*SearchMoviesRe
 		}
 
 		if len(moviesResp.Results) > 0 {
+			moviesResp.SortResults(chsTitle, engTitle, strYear)
 			utils.Logger.InfoF("search movies: %s %d result count: %d, use: %v", chsTitle, year, len(moviesResp.Results), moviesResp.Results[0])
 			return moviesResp.Results[0], nil
 		}
 	}
 
 	return nil, errors.New("search movie not found")
+}
+
+// SortResults sorts the movie search results based on multiple criteria
+func (resp *SearchMoviesResponse) SortResults(chsTitle, engTitle, year string) {
+	sort.SliceStable(resp.Results, func(i, j int) bool {
+		// Check for content completeness
+		completeI := isCompleteMovie(resp.Results[i])
+		completeJ := isCompleteMovie(resp.Results[j])
+		if completeI != completeJ {
+			return completeI
+		}
+
+		// If year is provided, prioritize results from that year
+		if year != "" {
+			yearInt, _ := strconv.Atoi(year)
+			yearI, _ := strconv.Atoi(resp.Results[i].ReleaseDate[:4])
+			yearJ, _ := strconv.Atoi(resp.Results[j].ReleaseDate[:4])
+			if yearI == yearInt && yearJ != yearInt {
+				return true
+			}
+			if yearI != yearInt && yearJ == yearInt {
+				return false
+			}
+		}
+
+		// Check for matching Chinese or English title
+		matchTitleI := matchesTitleMovie(resp.Results[i], chsTitle, engTitle)
+		matchTitleJ := matchesTitleMovie(resp.Results[j], chsTitle, engTitle)
+		if matchTitleI != matchTitleJ {
+			return matchTitleI
+		}
+
+		// Sort primarily by vote_average, then by popularity
+		if resp.Results[i].VoteAverage != resp.Results[j].VoteAverage {
+			return resp.Results[i].VoteAverage > resp.Results[j].VoteAverage
+		}
+		return resp.Results[i].Popularity > resp.Results[j].Popularity
+	})
+}
+
+// isCompleteMovie checks if the movie result has complete information
+func isCompleteMovie(result *SearchMoviesResults) bool {
+	return result.PosterPath != "" && result.BackdropPath != "" && result.Overview != ""
+}
+
+// matchesTitleMovie checks if the movie result matches the given Chinese or English title
+func matchesTitleMovie(result *SearchMoviesResults, chsTitle, engTitle string) bool {
+	return strings.Contains(result.Title, chsTitle) || strings.Contains(result.OriginalTitle, engTitle)
 }
--- tmdb/search_tv.go
+++ tmdb/search_tv.go
@@ -60,18 +60,66 @@ func swap(s []*SearchResults, i, j int) {
 
 // SortResults 按流行度排序
 // TODO 是否有点太粗暴了，考虑多维度：内容完整性、年份、中英文等
-func (d SearchTvResponse) SortResults(year string) {
-	// 考虑年份 年份对的优先级拉高
-	sort.Sort(SearchTvResultsSortWrapper{d.Results, func(l, r *SearchResults) bool {
-		return l.Popularity > r.Popularity
-	}})
-	for i := 0; i < len(d.Results); i++ {
-		firstAirDate := d.Results[i].FirstAirDate
-		if strings.Split(firstAirDate, "-")[0] == year {
-			swap(d.Results, 0, i)
-			break
+// func (d SearchTvResponse) SortResults(year string) {
+// 	// 考虑年份 年份对的优先级拉高
+// 	sort.Sort(SearchTvResultsSortWrapper{d.Results, func(l, r *SearchResults) bool {
+// 		return l.Popularity > r.Popularity
+// 	}})
+// 	for i := 0; i < len(d.Results); i++ {
+// 		firstAirDate := d.Results[i].FirstAirDate
+// 		if strings.Split(firstAirDate, "-")[0] == year {
+// 			swap(d.Results, 0, i)
+// 			break
+// 		}
+// 	}
+// }
+
+// SortResults sorts the TV search results based on multiple criteria
+func (resp *SearchTvResponse) SortResults(chsTitle, engTitle, year string) {
+	sort.SliceStable(resp.Results, func(i, j int) bool {
+		// Check for content completeness
+		completeI := isComplete(resp.Results[i])
+		completeJ := isComplete(resp.Results[j])
+		if completeI != completeJ {
+			return completeI
 		}
-	}
+
+		// If year is provided, prioritize results from that year
+		if year != "" {
+			yearInt, _ := strconv.Atoi(year)
+			yearI, _ := strconv.Atoi(resp.Results[i].FirstAirDate[:4])
+			yearJ, _ := strconv.Atoi(resp.Results[j].FirstAirDate[:4])
+			if yearI == yearInt && yearJ != yearInt {
+				return true
+			}
+			if yearI != yearInt && yearJ == yearInt {
+				return false
+			}
+		}
+
+		// Check for matching Chinese or English title
+		matchTitleI := matchesTitle(resp.Results[i], chsTitle, engTitle)
+		matchTitleJ := matchesTitle(resp.Results[j], chsTitle, engTitle)
+		if matchTitleI != matchTitleJ {
+			return matchTitleI
+		}
+
+		// Sort primarily by vote_average, then by popularity
+		if resp.Results[i].VoteAverage != resp.Results[j].VoteAverage {
+			return resp.Results[i].VoteAverage > resp.Results[j].VoteAverage
+		}
+		return resp.Results[i].Popularity > resp.Results[j].Popularity
+	})
+}
+
+// isComplete checks if the result has complete information
+func isComplete(result *SearchResults) bool {
+	return result.PosterPath != "" && result.BackdropPath != "" && result.Overview != ""
+}
+
+// matchesTitle checks if the result matches the given Chinese or English title
+func matchesTitle(result *SearchResults, chsTitle, engTitle string) bool {
+	return strings.Contains(result.Name, chsTitle) || strings.Contains(result.OriginalName, engTitle)
 }
 
 // SearchShows 搜索tmdb
@@ -132,7 +180,7 @@ func (t *tmdb) SearchShows(chsTitle, engTitle string, year int) (*SearchResults,
 		}
 
 		if len(tvResp.Results) > 0 {
-			tvResp.SortResults(strYear)
+			tvResp.SortResults(chsTitle, engTitle, strYear)
 			utils.Logger.InfoF("search tv: %s %d result count: %d, use: %v", chsTitle, year, len(tvResp.Results), tvResp.Results[0])
 			return tvResp.Results[0], nil
 		}
